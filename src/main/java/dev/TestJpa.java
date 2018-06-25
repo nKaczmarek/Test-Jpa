002e61911dd7283ce492910c45354f3609898861
Kaczmarek Noel <kaczmarek.no@gmail.com>
2018-06-25 11:11:51 +0200

    Ajouts JPA

---
+++ src/main/java/dev/TestJpa.java
@@ -0,0 +1,15 @@
+package dev;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+		EntityManager em = entityManagerFactory.createEntityManager();
+		
+	}
+
+}
